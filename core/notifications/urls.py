dfb3219cdc0d02bc4b85600e831dd74d3db6d32a
aycanmuratbekova <aycanmuratbekova@gmail.com>
2024-08-27 16:43:43 +0600

    modified:   README.md

--- core/notifications/urls.py
+++ core/notifications/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    # ... другие пути
     path('', TemplateView.as_view(template_name='index.html')),
 ]
